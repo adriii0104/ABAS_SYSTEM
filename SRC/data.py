4c287f2d5cdadf5e30b0409d27edd79cd27723ce
Lexesus <alexisreyes0112@hotmail.com>
2023-10-11 19:39:52 -0400

    update

--- SRC/data.py
+++ SRC/data.py
@@ -1,9 +1,10 @@
-import requests
+
 
 url = "http://127.0.0.1:5000/apirequest_data_abas_system"
 
 def Add_inventory(**kwargs):
     print(kwargs["quantity"])
+    print(kwargs["name_art"])
 
 
 #response = requests.post(url, json=data, verify=False)
